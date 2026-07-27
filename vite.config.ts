b3864b3e4102b246edf90a93fc189a777afce00c
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-27 12:03:46 +0700

    chore: initialize project

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import oxfmt from "@kekkon-nexus/config/oxfmt";
+import oxlint from "@kekkon-nexus/config/oxlint";
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+	fmt: {
+		...oxfmt,
+	},
+	lint: {
+		extends: [oxlint],
+		jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+
+		rules: {
+			"vite-plus/prefer-vite-plus-imports": "error",
+		},
+	},
+	staged: {
+		"*": "vp check --fix --no-error-on-unmatched-pattern",
+	},
+});
